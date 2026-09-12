9d8def08cd73d5b23231d73ecb1c076723723616
seifmomo <seifmomo@users.noreply.github.com>
2026-09-12 13:48:54 +0300

    Fix GitHub Pages: add /egydir/ base path, router basename, and gh-pages deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function ScrollToTop() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/egydir">
       <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
